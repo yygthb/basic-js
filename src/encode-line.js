7491f42f062ec1b1986926a531dcdad017e8b622
Yura Ya <yy.ainu.dev@gmail.com>
2022-04-28 12:37:10 +0300

    feat: encode lines

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (str === '') return str;
+
+  const letters = [];
+
+  for (let letter of str) {
+    if (letters[letters.length - 1] !== letter) {
+      letters.push(letter);
+    } else {
+      // console.log(
+      //   letters[letters.length - 2],
+      //   typeof parseInt(letters[letters.length - 2]),
+      //   parseInt(letters[letters.length - 2])
+      // );
+      if (isNaN(parseInt(letters[letters.length - 2]))) {
+        letters[letters.length - 1] = 2;
+        letters.push(letter);
+      } else {
+        letters[letters.length - 2]++;
+      }
+    }
+  }
+
+  return letters.join('');
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
